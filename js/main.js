5a63a13ed8df5ce0d615789980acf2a201524daa
Ulises Castaneda <76233367+UlisesGoetz@users.noreply.github.com>
2022-01-05 16:24:18 -0600

    Primera version

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.navbar').addClass('nav-sticky');
+        } else {
+            $('.navbar').removeClass('nav-sticky');
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            }
+        }
+    });
+    
+    
+    
+
+
